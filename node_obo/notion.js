7f87a077922030611ab79af7d04442911c5fdfbc
albona <albona@AB-maeda.local>
2024-07-11 03:51:20 +0900

    dotenvのインポート

--- node_obo/notion.js
+++ node_obo/notion.js
@@ -1,4 +1,8 @@
 import { Client } from "@notionhq/client";
+import dotenv from "dotenv";
+
+// 環境変数を読み込む
+dotenv.config();
 
 const notion = new NotionClient({ auth: process.env.NOTION_API_KEY });
 const taskDatabaseId = process.env.TASK_DATABASE_ID;
